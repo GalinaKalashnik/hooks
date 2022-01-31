7b52143d6139b6966297944e28cd27e018f50cd9
galaKalashnik <galina.kalashnik@balanceinternet.com.au>
2022-01-31 07:16:01 +0200

    96: useState

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+    return(
+        <div>
+            <HookSwitcher />
+        </div>
+    );
+}
+
+const HookSwitcher = () => {
+
+    //color - текущий цвет
+    //setColor - функция кот работает аналогично setState
+    const [ color, setColor ] = useState('white');
+    const [ fontSize, setFontSize ] = useState(14);
+
+    return (
+        <div style={{padding: '10px', border: '1px solid #000', backgroundColor: color, fontSize: `${fontSize}px`}}>
+            <p>Hello word</p>
+            <div style={{marginBottom: '10px'}}>
+                <button
+                    onClick={() => setColor('grey')}>
+                    Dark
+                </button>
+                <button
+                    onClick={() => setColor('white')}>
+                    Light
+                </button>
+            </div>
+
+            <div style={{marginBottom: '10px'}}>
+                <button
+                    onClick={() => setFontSize((s) => s + 2)}>
+                    +
+                </button>
+                <button
+                    onClick={() => setFontSize((s) => s - 2)}>
+                    -
+                </button>
+            </div>
+        </div>
+    );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +54,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
